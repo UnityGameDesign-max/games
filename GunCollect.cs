a764ac94d4c2445ef1d43309c4733499a7adb48b
Tumelo <55387517+UnityGameDesign-max@users.noreply.github.com>
2021-01-14 17:22:22 +0200

    Add files via upload

---
+++ GunCollect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunCollect : MonoBehaviour{
+
+    public GameObject SciGun;
+
+
+    void Start(){
+      
+    }
+
+    void OnTriggerEnter(Collider other) {
+        
+        SciGun.SetActive(true);
+        SciGun.GetComponent<Animation>().Play("GunTriggerAnim");
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ Shoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public AudioSource shoot;
+    private float betweenTimeAttack;
+    public float maxTimeAttack = 1.2f;
+    
+
+    void Update()
+    {
+        if(Input.GetMouseButton(0)){
+            this.GetComponent<Animation>().Play("TriggerAnim");
+            if(betweenTimeAttack > maxTimeAttack){
+                
+                shoot.Play();
+                betweenTimeAttack = 0;
+            }
+            betweenTimeAttack += Time.deltaTime;
+            
+        }
+    
+    }
+}
